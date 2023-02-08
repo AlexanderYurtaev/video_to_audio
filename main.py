6a686bc2746d8b29a6716d32cd2441dead4ddaf0
aleksandr <digitalpochta@mail.ru>
2023-02-08 18:16:30 +0300

    updated requirements.txt and created two functions

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
+from moviepy.editor import *
 
 
+# file_location = '/Users/aleksandr/Downloads/Архив с ютуба/Собеседование Python. Разбор вопросов.mp4'
+def video_to_audio():
+    file_location = input("Please enter a full path to file you want to convert to audio.")
+    print(f'file location is: {file_location}')
+    converted_file_name = create_converted_filename(file_location)
+    print(converted_file_name)
+    audioclip = AudioFileClip(file_location)
+    audioclip.write_audiofile(converted_file_name)
 
 
-
-
-
-
-
-
+def create_converted_filename(file_path: str) -> str:
+    slash_index = file_path.rindex('/')
+    dot_index = file_path.rindex('.')
+    return file_path[slash_index + 1:dot_index]
 
 # if __name__ == '__main__':
-
+video_to_audio()
