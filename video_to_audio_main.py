891a1b1d4082b7769620b5d47cdc4bbf04d4f65d
aleksandr <digitalpochta@mail.ru>
2023-02-20 21:41:49 +0300

    1234

--- video_to_audio_main.py
+++ video_to_audio_main.py
@@ -3,7 +3,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import Qt
 import sys
-from funcs import video_to_audio, download_video_from_youtube, cut_audio_file
+from funcs import video_to_audio, download_video_from_youtube, cut_audio_file, download_from_youtube
 
 
 class Window(QMainWindow):
@@ -262,8 +262,6 @@ class Window(QMainWindow):
 
 
 def start_application():
-    import funcs
-    import utils
     app = QApplication(sys.argv)
     window = Window()
     window.show()
